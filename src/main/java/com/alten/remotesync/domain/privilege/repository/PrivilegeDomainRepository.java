efd5b70c5ec00514c84dd84593191c8eb4ad5213
MohamedHakkouAlten <mohamed.hakkou@alten.com>
2025-03-24 14:29:44 +0000

    Refactor: Updating Repository Names

--- src/main/java/com/alten/remotesync/domain/privilege/repository/PrivilegeDomainRepository.java
+++ src/main/java/com/alten/remotesync/domain/privilege/repository/PrivilegeDomainRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PrivilegeRepository extends JpaRepository<Privilege, Long> {
+public interface PrivilegeDomainRepository extends JpaRepository<Privilege, Long> {
 }
