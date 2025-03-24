08aeb760dd1f9620714d43392366a02d72611fc1
Mohamed HAKKOU <mohamed.hakkou@alten.com>
2025-03-24 11:00:40 +0000

    Init RemoteSync Project
    Setup Environement

---
+++ src/test/java/com/alten/remotesync/RemoteSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alten.remotesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemotesyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
