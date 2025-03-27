4014bc294101c6c03ea8f5822231688b2e20259d
hjdaidi <hamza.jdaidi@alten.com>
2025-03-27 13:17:01 +0000

    feat: Enhance Project management with DTOs and service interface
    - Created `ProjectService.java` interface to define project-related operations.
    - Added `ClientIdDTO.java`, `ProjectCountDTO.java`, `ProjectDTO.java`, `ProjectIdDTO.java`, and `ProjectLabelDTO.java` for project data transfer between layers.
    - Implemented `projectMapper.java` to map between entities and DTOs for project-related data.
    - Developed unit tests in `TEST.java` to verify the functionality of project-related services.

--- src/main/java/com/alten/remotesync/application/client/service/ClientServiceImp.java
+++ src/main/java/com/alten/remotesync/application/client/service/ClientServiceImp.java
@@ -1,9 +1,11 @@
 package com.alten.remotesync.application.client.service;
 
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class ClientServiceImp implements ClientService {
 }
--- src/main/java/com/alten/remotesync/application/project/service/ProjectService.java
+++ src/main/java/com/alten/remotesync/application/project/service/ProjectService.java
@@ -1,7 +1,24 @@
 package com.alten.remotesync.application.project.service;
 
+import com.alten.remotesync.application.project.record.request.ClientIdDTO;
+import com.alten.remotesync.application.project.record.request.ProjectIdDTO;
+import com.alten.remotesync.application.project.record.request.ProjectLabelDTO;
+import com.alten.remotesync.application.project.record.response.ProjectCountDTO;
+import com.alten.remotesync.application.project.record.response.ProjectDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface ProjectService {
+
+    List<ProjectDTO> getAssociateProjects();
+
+    ProjectDTO getAssociateProjectDetails(ProjectIdDTO projectIdDTO);
+
+    List<ProjectDTO> getAssociateProjectsByLabel(ProjectLabelDTO projectLabelDTO);
+
+    ProjectCountDTO getAssociateProjectsCount();
+
+    List<ProjectDTO> getAssociateProjectsByClient(ClientIdDTO clientIdDTO);
 }
