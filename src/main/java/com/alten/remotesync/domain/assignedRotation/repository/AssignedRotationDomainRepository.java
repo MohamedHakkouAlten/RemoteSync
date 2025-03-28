d45380cc1842d8eea297b7eac7e34991ebbadec4
hjdaidi <hamza.jdaidi@alten.com>
2025-03-28 14:15:34 +0000

    feat: Implement project association and retrieval methods
    - Added `getAssociateOldProjects(PagedGlobalIdDTO)` for fetching old associated projects with pagination.
    - Implemented `getProjectDetails(GlobalDTO)` to retrieve project details based on global identifier.
    - Developed `getAssociateOldProjectsByLabel(AssociateProjectByLabelDTO)` to fetch old projects by label.
    - Added `getAssociateProjectsCount(GlobalDTO)` to retrieve the count of associated projects.
    - Implemented `getAssociateProjectsByClient()` for fetching associated projects by client.
    - Created necessary DTOs for request and response handling.

--- src/main/java/com/alten/remotesync/domain/assignedRotation/repository/AssignedRotationDomainRepository.java
+++ src/main/java/com/alten/remotesync/domain/assignedRotation/repository/AssignedRotationDomainRepository.java
@@ -2,11 +2,14 @@ package com.alten.remotesync.domain.assignedRotation.repository;
 
 import com.alten.remotesync.domain.assignedRotation.model.embeddable.AssignedRotationId;
 import com.alten.remotesync.domain.assignedRotation.model.AssignedRotation;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface AssignedRotationDomainRepository extends JpaRepository<AssignedRotation, AssignedRotationId> {
+
 
 
 }
