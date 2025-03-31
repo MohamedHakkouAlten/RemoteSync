ff82bab665fd904116f80f2a0c5f8c3453a11898
MohamedHakkouAlten <mohamed.hakkou@alten.com>
2025-03-31 19:53:55 +0000

    Refactor and update service classes, DTOs, mappers, and exception handling
    - Refactored and added service implementations for AssignedRotation, Project, Report, and User entities.
    - Updated, created new DTOs, mappers.
    - Improved exception handling across various services (e.g., AssignedRotationNotFoundException, ProjectNotFoundException, UserNotFoundException).
    - Enhanced authentication, and response wrapping mechanisms.

--- src/main/java/com/alten/remotesync/application/user/service/UserServiceImp.java
+++ src/main/java/com/alten/remotesync/application/user/service/UserServiceImp.java
@@ -1,5 +1,7 @@
 package com.alten.remotesync.application.user.service;
 
+import com.alten.remotesync.adapter.exception.user.UserNotFoundException;
+import com.alten.remotesync.application.globalDTO.GlobalDTO;
 import com.alten.remotesync.application.user.mapper.UserMapper;
 import com.alten.remotesync.application.user.record.request.LoginRequestDTO;
 import com.alten.remotesync.application.user.record.request.UpdateUserProfileDTO;
@@ -7,73 +9,50 @@ import com.alten.remotesync.application.user.record.response.LoginResponseDTO;
 import com.alten.remotesync.application.user.record.response.UserProfileDTO;
 import com.alten.remotesync.domain.user.model.User;
 import com.alten.remotesync.domain.user.repository.UserDomainRepository;
-import com.alten.remotesync.domain.user.services.UserDetailsServiceImp;
 import com.alten.remotesync.kernel.security.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.UUID;
-
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class UserServiceImp implements UserService {
-
     private final UserDomainRepository userDomainRepository;
     private final UserMapper userMapper;
-    private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
-    private final UserDetailsServiceImp userDetailsService;
 
     @Override
-    public UserProfileDTO getMyProfile(UUID userId) {
-        User user = userDomainRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found with id: " + userId));
-        return userMapper.toUserProfileDTO(user);
+    public UserProfileDTO getMyProfile(GlobalDTO globalDTO) {
+        User dbuser = userDomainRepository.findById(globalDTO.userId())
+                .orElseThrow(() -> new UserNotFoundException("User not found with id: " + globalDTO.userId()));
+        return userMapper.toUserProfileDTO(dbuser);
     }
 
     @Override
-    @Transactional
-    public UserProfileDTO updateMyProfile(UUID userId, UpdateUserProfileDTO updateUserProfileDTO) {
-        User user = userDomainRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found with id: " + userId));
+    public UserProfileDTO updateMyProfile(UpdateUserProfileDTO updateUserProfileDTO) {
+        User dbUser = userDomainRepository.findById(updateUserProfileDTO.userId())
+                .orElseThrow(() -> new UserNotFoundException("User not found with id: " + updateUserProfileDTO.userId()));
 
-        if (updateUserProfileDTO.password() != null) {
-            user.setPassword(passwordEncoder.encode(updateUserProfileDTO.password()));
-        }
+        dbUser.setFirstName(updateUserProfileDTO.firstName());
+        dbUser.setLastName(updateUserProfileDTO.lastName());
+        dbUser.setEmail(updateUserProfileDTO.email());
+        dbUser.setPhoneNumber(updateUserProfileDTO.phoneNumber());
 
-        user.setFirstName(updateUserProfileDTO.firstName());
-        user.setLastName(updateUserProfileDTO.lastName());
-        user.setEmail(updateUserProfileDTO.email());
-        user.setPhoneNumber(updateUserProfileDTO.phoneNumber());
-
-        User updatedUser = userDomainRepository.save(user);
-        return userMapper.toUserProfileDTO(updatedUser);
+        return userMapper.toUserProfileDTO(userDomainRepository.save(dbUser));
     }
 
     @Override
     public LoginResponseDTO login(LoginRequestDTO loginRequestDTO) {
-        try {
-            UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(loginRequestDTO.username(), loginRequestDTO.password());
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequestDTO.username(), loginRequestDTO.password()));
 
-            authenticationManager.authenticate(authenticationToken);
-            UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequestDTO.username());
-            User user = userDomainRepository.findByUsername(loginRequestDTO.username());
-            if (user == null) {
-                throw new RuntimeException("User not found");
-            }
-            String accessToken = jwtService.generateAccessToken(user);
+        if(!authentication.isAuthenticated()) throw new UserNotFoundException("Authentication failed user was not found");
 
-            return new LoginResponseDTO(accessToken);
-        } catch (BadCredentialsException e) {
-            throw new RuntimeException("Invalid credentials", e);
-        }
+        User dbUser = userDomainRepository.findByUsername(loginRequestDTO.username());
+
+        return new LoginResponseDTO(jwtService.generateAccessToken(dbUser), jwtService.generateRefreshToken(dbUser));
     }
 }
