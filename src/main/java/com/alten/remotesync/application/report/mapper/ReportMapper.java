9dc28205bc56865b75505a66c533b12c9f483a4e
MohamedHakkouAlten <mohamed.hakkou@alten.com>
2025-03-31 19:53:55 +0000

    Refactor and update service classes, DTOs, mappers, and exception handling
    - Refactored and added service implementations for AssignedRotation, Project, Report, and User entities.
    - Updated, created new DTOs, mappers.
    - Improved exception handling across various services (e.g., AssignedRotationNotFoundException, ProjectNotFoundException, UserNotFoundException).
    - Enhanced authentication, and response wrapping mechanisms.

--- src/main/java/com/alten/remotesync/application/report/mapper/ReportMapper.java
+++ src/main/java/com/alten/remotesync/application/report/mapper/ReportMapper.java
@@ -1,6 +1,6 @@
 package com.alten.remotesync.application.report.mapper;
 
-import com.alten.remotesync.application.report.record.request.AssociateReportDTO;
+import com.alten.remotesync.application.report.record.request.CreateAssociateReportDTO;
 import com.alten.remotesync.application.report.record.response.ReportDTO;
 import com.alten.remotesync.domain.report.model.Report;
 import org.mapstruct.Mapper;
@@ -8,5 +8,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface ReportMapper {
     ReportDTO toReportDTO(Report report);
-    Report toAssociateReport(AssociateReportDTO associateReportDTO);
+    Report toAssociateReport(CreateAssociateReportDTO createAssociateReportDTO);
 }
