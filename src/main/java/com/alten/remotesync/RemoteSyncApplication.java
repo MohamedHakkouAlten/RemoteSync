349317bbc4dfebb2beed52a22ad1246608aa3bce
MohamedHakkouAlten <mohamed.hakkou@alten.com>
2025-04-01 09:59:21 +0000

    Enhance error handling, validation, and user tracking
    - Updated DTOs with improved validation annotations.
    - Added custom error response handling in ResponseWrapper for validation errors with additional context (timestamp, triggeredBy).
    - Implemented enhanced error handling in GlobalExceptionHandler to capture validation exceptions and include user information and timestamps.

--- src/main/java/com/alten/remotesync/RemoteSyncApplication.java
+++ src/main/java/com/alten/remotesync/RemoteSyncApplication.java
@@ -1,18 +1,12 @@
 package com.alten.remotesync;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class RemoteSyncApplication {
     public static void main(String[] args) {
         SpringApplication.run(RemoteSyncApplication.class, args);
     }
-
-
-
-
 }
