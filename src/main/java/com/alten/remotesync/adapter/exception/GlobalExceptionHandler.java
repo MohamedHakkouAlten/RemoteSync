74f8b6dfaa53482fcbfddace7f414f5ea617baae
MohamedHakkouAlten <mohamed.hakkou@alten.com>
2025-04-01 09:59:21 +0000

    Enhance error handling, validation, and user tracking
    - Updated DTOs with improved validation annotations.
    - Added custom error response handling in ResponseWrapper for validation errors with additional context (timestamp, triggeredBy).
    - Implemented enhanced error handling in GlobalExceptionHandler to capture validation exceptions and include user information and timestamps.

--- src/main/java/com/alten/remotesync/adapter/exception/GlobalExceptionHandler.java
+++ src/main/java/com/alten/remotesync/adapter/exception/GlobalExceptionHandler.java
@@ -7,38 +7,69 @@ import com.alten.remotesync.adapter.exception.user.UserNotFoundException;
 import com.alten.remotesync.adapter.wrapper.ResponseWrapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Object> userNotFoundException(UserNotFoundException e) {
+    public ResponseEntity<?> userNotFoundException(UserNotFoundException e) {
         Map<String, Object> response = ResponseWrapper.error(e.getMessage(), HttpStatus.NOT_FOUND);
         // ADDING LOGS HERE FOR THE DATABASE !
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ProjectNotFoundException.class)
-    public ResponseEntity<Object> projectNotFoundException(ProjectNotFoundException e) {
+    public ResponseEntity<?> projectNotFoundException(ProjectNotFoundException e) {
         Map<String, Object> response = ResponseWrapper.error(e.getMessage(), HttpStatus.NOT_FOUND);
         // ADDING LOGS HERE FOR THE DATABASE !
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AssignedRotationNotFoundException.class)
-    public ResponseEntity<Object> assignedRotationNotFoundException(AssignedRotationNotFoundException e) {
+    public ResponseEntity<?> assignedRotationNotFoundException(AssignedRotationNotFoundException e) {
         Map<String, Object> response = ResponseWrapper.error(e.getMessage(), HttpStatus.NOT_FOUND);
         // ADDING LOGS HERE FOR THE DATABASE !
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ReportNotFoundException.class)
-    public ResponseEntity<Object> reportNotFoundException(ReportNotFoundException e) {
+    public ResponseEntity<?> reportNotFoundException(ReportNotFoundException e) {
         Map<String, Object> response = ResponseWrapper.error(e.getMessage(), HttpStatus.NOT_FOUND);
         // ADDING LOGS HERE FOR THE DATABASE !
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // VALIDATION API RESPONSE
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String triggeredBy = (authentication != null) ? authentication.getName() : "Unknown";
+
+        Map<String, Object> response = ResponseWrapper.validationError(fieldErrors, HttpStatus.BAD_REQUEST, triggeredBy);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/alten/remotesync/application/assignedRotation/record/response/TEST.java
+++
@@ -1,4 +0,0 @@
-package com.alten.remotesync.application.assignedRotation.record.response;
-
-public class TEST {
-}
