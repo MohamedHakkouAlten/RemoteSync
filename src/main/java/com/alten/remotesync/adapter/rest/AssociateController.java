28164ace168f07346527ece4235a3ad90235835c
maryamalaoui2001 <maryam.alaouiismaili@alten.com>
2025-04-01 14:47:10 +0000

    login and profile controllers

--- src/main/java/com/alten/remotesync/adapter/rest/AssociateController.java
+++ src/main/java/com/alten/remotesync/adapter/rest/AssociateController.java
@@ -1,16 +1,28 @@
 package com.alten.remotesync.adapter.rest;
 
+import com.alten.remotesync.application.globalDTO.GlobalDTO;
+import com.alten.remotesync.application.user.record.request.LoginRequestDTO;
+import com.alten.remotesync.application.user.record.request.UpdateUserProfileDTO;
+import com.alten.remotesync.application.user.record.response.LoginResponseDTO;
+import com.alten.remotesync.application.user.record.response.UserProfileDTO;
+import com.alten.remotesync.application.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class AssociateController {
-    @GetMapping({"/associate/test", "/admin/test"})
-    public String s(){
-        return "HAPPY";
+    private final UserService userService;
+
+    @GetMapping("/associate/{userId}")
+    public ResponseEntity<UserProfileDTO> getAssociateProfile(@PathVariable UUID userId) {
+        UserProfileDTO profile = userService.getMyProfile(GlobalDTO.fromUserId(userId));
+        return ResponseEntity.ok(profile);
     }
+
 }
