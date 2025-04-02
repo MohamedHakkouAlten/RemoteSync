9273a80af19b86a857fdcf959da354db49d7cdb2
hjdaidi <hamza.jdaidi@alten.com>
2025-04-02 12:11:39 +0000

    refactor:removed unused imports RemoteSyncApplicationTests.java

--- src/test/java/com/alten/remotesync/RemoteSyncApplicationTests.java
+++ src/test/java/com/alten/remotesync/RemoteSyncApplicationTests.java
@@ -1,27 +1,10 @@
 package com.alten.remotesync;
 
-import com.alten.remotesync.application.globalDTO.GlobalDTO;
-import com.alten.remotesync.application.globalDTO.PagedGlobalIdDTO;
-import com.alten.remotesync.application.project.record.request.AssociateProjectByLabelDTO;
-import com.alten.remotesync.application.project.record.response.PagedProjectDTO;
-import com.alten.remotesync.application.project.record.response.ProjectDTO;
-import com.alten.remotesync.application.project.service.ProjectService;
-import com.alten.remotesync.domain.assignedRotation.enumeration.RotationAssignmentStatus;
-import com.alten.remotesync.domain.assignedRotation.model.AssignedRotation;
-import com.alten.remotesync.domain.assignedRotation.repository.AssignedRotationDomainRepository;
-import com.alten.remotesync.domain.user.repository.UserDomainRepository;
-import org.hamcrest.Matcher;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class RemotesyncApplicationTests {
