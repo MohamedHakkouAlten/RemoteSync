0a3495185651f77b0d43b5e0c0b48b949ec898a6
hjdaidi <hamza.jdaidi@alten.com>
2025-04-02 11:59:16 +0000

    refactor: refactored AssociateController.java and removed test from RemoteSyncApplicationTests.java

--- src/test/java/com/alten/remotesync/RemoteSyncApplicationTests.java
+++ src/test/java/com/alten/remotesync/RemoteSyncApplicationTests.java
@@ -26,53 +26,13 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest
 class RemotesyncApplicationTests {
 
-	@Autowired
-	private ProjectService projectService;
-	@Autowired
-	private UserDomainRepository userDomainRepository;
-	@Autowired
-	private AssignedRotationDomainRepository assignedRotationDomainRepository;
+
+
 	@Test
 	void contextLoads() {
 	}
 
-	@Test
-	void TestGetProjectsCountByUser(){
-		UUID userId=UUID.fromString("e9f0a1b2-c3d4-e5f6-7890-a7d8e9a1b2c3");
-		assertEquals(2,projectService.getAssociateProjectsCount(GlobalDTO.fromUserId(userId)).projectsCount());
-	}
-	@Test
-	void TestSearchAssociateProjects(){
-		UUID userId=UUID.fromString("e9f0a1b2-c3d4-e5f6-7890-a7d8e9a1b2c3");
-		List<ProjectDTO> projectDTOS=projectService.getAssociateOldProjectsByLabel(new AssociateProjectByLabelDTO("Project",userId, 0,null)).projectDTOS();
-		assertEquals(1,projectDTOS.size());
-		assertNotNull(projectDTOS.get(0).owner());
 
-	}
-	@Test
-	void TestGetAssociateProjects(){
-		UUID userId=UUID.fromString("e9f0a1b2-c3d4-e5f6-7890-a7d8e9a1b2c3");
-		List<ProjectDTO> projectDTOS= projectService.getAssociateOldProjects(new PagedGlobalIdDTO(GlobalDTO.fromUserId(userId),0,10)).projectDTOS();
-		assertEquals(1,projectDTOS.size());
-		assertNotNull(projectDTOS.get(0).owner());
-
-	}
-	@Test
-	void TestGetProjectDetails(){
-		UUID projectId=UUID.fromString("a6d7e8f7-8901-b2c3-d4e5-f67890a7d8e9");
-		ProjectDTO projectDTO= projectService.getProjectDetails(GlobalDTO.fromProjectId(projectId));
-		assertNotNull(projectDTO);
-
-	}
-	@Test
-	void TestGetProjectsByClient(){
-		UUID userId=UUID.fromString("e9f0a1b2-c3d4-e5f6-7890-a7d8e9a1b2c3");
-		UUID clientId=UUID.fromString("e5f67890-a5d6-e7f7-8901-b2c3d4e5f678");
-		PagedProjectDTO projectDTO= projectService.getAssociateProjectsByClient(new PagedGlobalIdDTO(GlobalDTO.fromUserIdAndClientId(userId,clientId),0,10));
-		System.out.println(projectDTO.projectDTOS().get(0).owner());
-		assertThat(projectDTO.projectDTOS().size()).isGreaterThan(0);
-
-	}
 
 
 
