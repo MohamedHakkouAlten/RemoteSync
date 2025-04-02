19b1857cc490bc590d8a645fc1d9b4aad4954b3b
MohamedHakkouAlten <mohamed.hakkou@alten.com>
2025-04-02 15:23:39 +0000

    Refactor and update various service and controller files
    - Updated AssignedRotationDomainRepository.java and AssignedRotationServiceImp.java.
    - Refactored AssociateController.java and AuthController.java for better request handling.
    - Enhanced ProjectServiceImp.java and added necessary changes to support new features.
    - Move RequestInterceptorConfig.java to new package.

--- src/main/java/com/alten/remotesync/domain/assignedRotation/repository/AssignedRotationDomainRepository.java
+++ src/main/java/com/alten/remotesync/domain/assignedRotation/repository/AssignedRotationDomainRepository.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 @Repository
 public interface AssignedRotationDomainRepository extends JpaRepository<AssignedRotation, AssignedRotationId> {
     Optional<AssignedRotation> findAssignedRotationByUser_UserIdAndProject_ProjectIdAndRotationAssignmentStatus(UUID user_userId, UUID project_projectId, RotationAssignmentStatus rotationAssignmentStatus, Sort sort);
-    Optional<List<AssignedRotation>> findAllAssignedRotationByUser_UserIdAndProject_ProjectIdAndRotationAndRotationAssignmentStatus(UUID user_userId, UUID project_projectId, RotationAssignmentStatus rotationAssignmentStatus, Sort sort);
+    Optional<List<AssignedRotation>> findAllAssignedRotationByUser_UserIdAndProject_ProjectIdAndRotationAssignmentStatus(UUID user_userId, UUID project_projectId, RotationAssignmentStatus rotationAssignmentStatus, Sort sort);
     Optional<AssignedRotation> findAssignedRotationByUser_UserIdAndProjectIsNullAndRotationAssignmentStatus(UUID user_userId, RotationAssignmentStatus rotationAssignmentStatus, Sort sort);
-    Optional<List<AssignedRotation>> findAllAssignedRotationByUser_UserIdAndRotationAndRotationAssignmentStatus(UUID user_userId, RotationAssignmentStatus rotationAssignmentStatus, Sort sort);
+    Optional<List<AssignedRotation>> findAllAssignedRotationByUser_UserIdAndRotationAssignmentStatus(UUID user_userId, RotationAssignmentStatus rotationAssignmentStatus, Sort sort);
 }
